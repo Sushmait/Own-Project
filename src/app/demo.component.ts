29ebe25fd540d9a429dd74dc71a06855ab3f973a
Arun94-HD <arunkumar.hd4@gmail.com>
2022-09-23 23:56:22 +0530

    I have created customized component

---
+++ src/app/demo.component.ts
@@ -0,0 +1,16 @@
+
+import { Component} from "@angular/core";
+
+@Component({
+    selector:"my-para-comp",
+    templateUrl: "./demo.component.html",
+    styleUrls:["./demo.component.css"]
+
+
+})
+
+
+
+export class DemoComponent{
+
+}
