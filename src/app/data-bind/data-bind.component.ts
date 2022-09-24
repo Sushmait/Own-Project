1e8e2dc0c13daea83ab46d7106c4a9379bd2f2f9
Arun94-HD <arunkumar.hd4@gmail.com>
2022-09-24 18:45:17 +0530

    customized comp creation & databinding with examples

---
+++ src/app/data-bind/data-bind.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-bind',
+  templateUrl: './data-bind.component.html',
+  styleUrls: ['./data-bind.component.css']
+})
+export class DataBindComponent  {
+
+'controlNumber'=100;
+ControlType=""
+
+  constructor() {
+    this.control();}
+
+  control(){switch (this.controlNumber)
+    {case 100:{
+      this.ControlType ="radio";}
+  break;
+
+  case 200:{this.ControlType="checkbox"}
+  break;}
+}
+  
+   
+ CourseName="Angular";
+ CountryName="India"
+ CityName="Bengaluru"
+ newspapername(){return "Indian express"}
+
+
+ Universityname="University of Cambridge"
+ Location="Mysore";
+
+
+
+}
