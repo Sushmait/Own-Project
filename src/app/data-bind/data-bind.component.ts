3f7709fe22da0a7fa39d1aaf87a8edaac5db49c3
Arun94-HD <arunkumar.hd4@gmail.com>
2022-09-29 18:26:18 +0530

    Property DB & Event DB together, practice of ngmodule

--- src/app/data-bind/data-bind.component.ts
+++ src/app/data-bind/data-bind.component.ts
@@ -5,36 +5,66 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './data-bind.component.html',
   styleUrls: ['./data-bind.component.css']
 })
-export class DataBindComponent  {
+export class DataBindComponent {
+  [x: string]: any;
 
-'controlNumber'=100;
-ControlType=""
-
+  'controlNumber' = 100;
+  'ControlType' = "text"
   constructor() {
-    this.control();}
+    //!this.control();//
+  }
+  control() {
+    switch (this.controlNumber) {
+      case 100: {
+        this.ControlType = "radio";
+      }
+        break;
 
-  control(){switch (this.controlNumber)
-    {case 100:{
-      this.ControlType ="radio";}
-  break;
+      case 200: { this.ControlType = "checkbox" }
+        break;
+      default: {
+        this.ControlType = "text"
+      }
+    }
+  }
+  CourseName = "Angular";
+  CountryName = "India"
+  CityName = "Bengaluru"
+  newspapername() { return "Indian express" }
+  Universityname = "University of Cambridge"
+  Location = "Mysore";
 
-  case 200:{this.ControlType="checkbox"}
-  break;}
-}
-  
-   
- CourseName="Angular";
- CountryName="India"
- CityName="Bengaluru"
- newspapername(){return "Indian express"}
-
-
- Universityname="University of Cambridge"
- Location="Mysore";
-
- Fired(){
+  Fired() {
     console.log("Successfully Completed the task & Happy clicking");
+    console.log("Happy Deepavali");
 
+    '"evtControlCheck()"'
+    this.control();
+  }
 
-  console.log("Happy Deepavali");
-}}
+  //customerName="Sushma;"//
+  //updatedCustomerName(){//
+  //this.customerName="Sushma DS";//
+  // console.log(this.customerName);}}//
+
+  Name = "Angular Course Training"
+  ShowMessage(message: HTMLInputElement) {
+    console.log(message.value)
+    console.log("showing property DB & Event DB together")
+  }
+
+  Skill = "Key Details abt the Event"
+  evtmessage(evtDetails: any) {
+    console.log(evtDetails);
+  }
+
+  Role = "Deputy Manager"
+  evtRole(Updatedrecords: any) {
+    console.log(Updatedrecords)
+    this.Role = Updatedrecords
+  }
+//console.log("triggering the event through ngModel");
+
+MicrosoftOwnedby = "Bill Gates"
+}
+
